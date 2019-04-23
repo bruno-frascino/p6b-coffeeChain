58092912b167c3eda22450fe8104f7506e8e76c2
Bruno Frascino <bfrascino80@gmail.com>
2019-04-23 16:32:50 +1000

    feat: Added rinkeby network settings for migration

--- truffle.js
+++ truffle.js
@@ -1,9 +1,20 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+ const mnemonic = "two erode truly claim album fly salad wagon book urban crawl coral"
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function(){
+        return new HDWalletProvider(mnemonic, 
+          "https://rinkeby.infura.io/v3/1d68e852f03c4360be7073c383111b22");
+      },
+      network_id: 4,
     }
   }
+  
 };
